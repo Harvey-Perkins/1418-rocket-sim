3111386fe021414c7db9a40065b528618349a6b7
Harvey-Perkins <harvey_perkins@my.cuesta.edu>
2018-12-14 12:13:38 -0800

    Setup for comparison with different velocity calculations

--- rocket_sim/simulation.py
+++ rocket_sim/simulation.py
@@ -1,7 +1,6 @@
 '''
-This isn't permanant, just a demo of the velocity verlet and how it can calculate velocity and position from a acceleration vector.
-In this case, it just simulates dropping an object from a certain height.
-I think we will sum forces on the rocket, use the fact that F=MA (F/M = A) to calculate the acceleration vector, then run that through the verlet.
+TODO:
+Make a proper test setup for this program
 
 '''
 
@@ -22,7 +21,7 @@ file_path = "../data/load/thrustcurve/AeroTech_D21.rse"
 #               x,y,z
 g0 = np.array([0.0,0.0,-9.81])
 acc = np.array([0.0,0.0,0.0])
-start_position = np.array([0.0,0.0,20.0])
+start_position = np.array([0.0,0.0,0.0])
 start_velocity = np.array([0.0,0.0,0.0])
 t = 0
 dt = 0.01
@@ -35,9 +34,11 @@ X = 0 #Makes accessing vector parts easier
 Y = 1
 Z = 2
 
+#Initialize parts
 rocket = mr.Rocket(start_position, start_velocity)
-structure1 = ms.Structure(0.8)
+structure1 = ms.Structure(0.0736)
 engine1 = me.Engine(file_path, np.array([0,0,1]), 1000)
+#Stick them on the rocket
 rocket.add_engine(engine1, np.array([0,0,-1.0]), np.array([0,0,0.05])) #move CoT to instance creation
 rocket.add_structure(structure1, np.array([0.0,0.0,0.0]))
 
@@ -49,12 +50,12 @@ ax2 = ax1.twinx()
 ax2.set_ylabel("Thrust (N)")
 fig.tight_layout()
 
-while rocket.position[Z] > 0:
+while rocket.position[Z] >= 0:
     #acc = np.array([0.0,0.0,0.0]) #reset acceleration
     #acc += g0 #add gravity to acceleration. Later sum all acceleration forces
-    if round(t, 10) == 1.58: #Ignition command
+    if rocket.position[Z] == 0: #Ignition command
         ax1.annotate('Ignition command', xy=(t, rocket.position[Z]), xytext=(2, 12),arrowprops=dict(facecolor='black', shrink=0)) #plots arrow pointing to when the ignition command is sent
-        engine1.ignite(0.1, t)
+        engine1.ignite(0, t)
     rocket.update(t, dt)
 
     rocket.position += rocket.velocity * dt + 0.5 * rocket.acceleration * dt * dt #Just stealing this from someone else's implementation of the velocity verlet...
@@ -63,6 +64,7 @@ while rocket.position[Z] > 0:
     #print(rocket.mass)
     #print(rocket.thrusts)
 
+    #Graphing stuff
     xs.append(t)
     thrustY.append(np.linalg.norm(rocket.thrusts))
     ys.append(rocket.position[Z])
